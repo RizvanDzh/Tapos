400aa3c381d6e2028a6ed0a534e3bac6bb67ef49
Rizvan Dzhamaludinov <rdzhamaludinov@fil-it.ru>
2024-07-31 14:19:36 +0300

    layout for otif tabs

--- libs/tapos/otif/features/feature-otif/src/lib/feature-otif/feature-otif.component.ts
+++ libs/tapos/otif/features/feature-otif/src/lib/feature-otif/feature-otif.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeatureTabsComponent } from '@tapos/feature-tabs';
 
 @Component({
     selector: 'tapos-feature-otif',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FeatureTabsComponent],
     templateUrl: './feature-otif.component.html',
     styleUrl: './feature-otif.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
