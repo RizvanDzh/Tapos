a614252dc5635129ea4b4de759962b7f322c4033
Rizvan Dzhamaludinov <rdzhamaludinov@fil-it.ru>
2025-03-02 16:13:57 +0300

    grouping complex data structures into FormArray

--- libs/tapos/pet/features/feature-reactive-form/src/lib/feature-reactive-form/components/reactive-form/reactive-form.component.ts
+++ libs/tapos/pet/features/feature-reactive-form/src/lib/feature-reactive-form/components/reactive-form/reactive-form.component.ts
@@ -1,12 +1,58 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { mergeMap, Observable, of } from 'rxjs';
 
 @Component({
     selector: 'tapos-reactive-form',
     standalone: true,
-    imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './reactive-form.component.html',
     styleUrl: './reactive-form.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ReactiveFormComponent {}
+export class ReactiveFormComponent implements OnInit {
+
+  public phoneLabels:string[] = ['Main', 'Home', 'Work'];
+
+  // eslint-disable-next-line @typescript-eslint/typedef
+  public form = new FormGroup({
+    firstName: new FormControl('Rizvan'),
+    lastName: new FormControl('Dzhamaludinov'),
+    nickname: new FormControl('Rizotto6'),
+    email: new FormControl('1234@gmail.com'),
+    yearOfBirth: new FormControl(2025),
+    passport: new FormControl(''),
+    address: new FormGroup({
+      fullAddress: new FormControl(''),
+      city: new FormControl(''),
+      postcode: new FormControl(0),
+    }),
+    phones: new FormArray([
+      new FormGroup({
+        label: new FormControl(this.phoneLabels[0]),
+        phoneNumber: new FormControl(''),
+      })
+    ])
+  });
+
+  public get years(): number[] {
+    const now: number = new Date().getUTCFullYear()
+    return Array(now - (now - 40)).fill('').map((_: number, idx: number) => now - idx)
+  }
+
+  public ngOnInit(): void {
+    console.log('ngOnInit reactive form');
+  }
+
+  public addPhone(): void {
+    this.form.controls.phones.insert(0, new FormGroup({
+      label: new FormControl(this.phoneLabels[0]),
+      phoneNumber: new FormControl(''),
+    }));
+  }
+
+  public removePhone(index: number): void {
+    this.form.controls.phones.removeAt(index);
+  }
+}
